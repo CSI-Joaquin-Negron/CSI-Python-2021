723cb72bdb4b4a6c9818108f474da4b06f3e1acd
CSI-Joaquin-Negron <89479739+CSI-Joaquin-Negron@users.noreply.github.com>
2021-09-10 14:26:05 -0400

    Meals completed

---
+++ Modules/Module4/Module4.py
@@ -0,0 +1,9 @@
+print("What was your breakfast today? ")
+meal = "pancakes"
+print(meal)
+print("What was your lunch?")
+meal = "Burrito with Nachos."
+print(meal)
+print("Have you had dinner?")
+meal = "Yes! Spaghetti with brocoli."
+print(meal)
