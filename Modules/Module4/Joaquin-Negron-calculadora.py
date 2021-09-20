32b38f2ecaf8981013a44dbf058083e136a5a869
CSI-Joaquin-Negron <89479739+CSI-Joaquin-Negron@users.noreply.github.com>
2021-09-20 14:47:53 -0400

    Calculadora

---
+++ Modules/Module4/Joaquin-Negron-calculadora.py
@@ -0,0 +1,9 @@
+myDistance = int(input('what is the distance?: '))
+myTime = int(input("what is the time?: "))
+print("The speed based of your numbers is", myDistance / myTime)
+
+def Speed(distance, time):
+    print(f"the speed is {int(distance) / int(time)}")
+print('the distance is 60')
+print('the time is 40')
+Speed(60,40)
