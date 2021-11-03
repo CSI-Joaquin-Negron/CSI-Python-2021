21bc1c0006b9073469a91f1e6b09c0cccdf599d4
CSI-Joaquin-Negron <89479739+CSI-Joaquin-Negron@users.noreply.github.com>
2021-11-03 14:54:34 -0400

    p4

--- Projects/Projectile-Motion/Projectile-Motion-Joaquin-Negron.py
+++ Projects/Projectile-Motion/Projectile-Motion-Joaquin-Negron.py
@@ -1,19 +1,39 @@
 import math
+from ExpetimentalData import ExperimentalData
 
-weapon = 'mosin'
-mosinCartridge = '7.62x54R LPS gzh'
-ammunition = 'single'
-velocity_ms = 865
+# weapon = 'mosin'
+# mosinCartridge = '7.62x54R LPS gzh'
+# ammunition = 'single'
+# velocity_ms = 865
 
-myBuilding = 'Adventureland'
-Adventureland = 70
-gravity = 9.81
+# myBuilding = 'Adventureland'
+# Adventureland = 70
+# gravity = 9.81
 
-def projectile_function(weapon:str, mosinCartridge:str, ammunition:str, velocity_ms:int, myBuilding:str, Adventureland:int, gravity:int):
+# convert your variables into parameters
+
+def projectile_function(experimentalData:ExperimentalData):
     time = math.sqrt((2 * Adventureland) / gravity)
-    distance = velocity_ms * time
+    # distance = (experimentalData[velocity_ms] * time)
 
     print(f'I chose the {weapon} for this experiment. The experiment requires the cartridge, {mosinCartridge}, the ammunition, {ammunition}, and the velocity, {velocity_ms}RPM.')
     print(f'After gathering this information, I next needed to choose a building. After picking {myBuilding}, i found the height {Adventureland} and measured the time   which was {time}. Finally, I needed to caculate the distance by multiplying velocity and time. The Distance of the bullet was {distance}')
 
-projectile_function('mosin', '7.62x54R LPS gzh', 'single', 865, 'Adventureland', 70, 9.81)
+# convert your script parameter into a json object.
+
+# experimentalData = {
+
+# "weapon": 'mosin',
+# "mosinCartridge": '7.62x54R LPS gzh',
+# 'ammunition': 'single',
+# 'velocity_ms': 865,
+
+# 'myBuilding': 'Adventureland',
+# 'Adventureland': 70,
+# 'gravity': 9.81
+    
+# }
+
+experimentalData = ExperimentalData('mosin', '7.62x54R LPS gzh', 'single', 865, 'Adventureland', 70, 9.81)
+
+projectile_function(experimentalData)
