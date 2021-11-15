879c44a4a44e4b3ce22bcc2b7311e8e4b33a6d75
CSI-Joaquin-Negron <89479739+CSI-Joaquin-Negron@users.noreply.github.com>
2021-11-15 14:30:55 -0400

    projectile motion completo

--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -21,7 +21,7 @@ def projectile_function(experimentalData:ExperimentalData):
     print(f'I chose the {experimentalData.weapon} for this experiment. The experiment requires the cartridge, {experimentalData.mosinCartridge}, the ammunition, {experimentalData.ammunition}, and the velocity, {experimentalData.velocity_ms}RPM.')
     print(f'After gathering this information, I next needed to choose a building. After picking {experimentalData.myBuilding}, i found the height {experimentalData.height} and measured the time   which was {time}. Finally, I needed to caculate the distance by multiplying velocity and time. The Distance of the bullet was {distance} and I added the gravities of Earth, Saturn, Mars, Moon and Jupiter in that order.')
 
-# convert your script parameter into a json object.
+# convert your script parameter into a json object..
 
 # experimentalData = {
 
@@ -38,6 +38,8 @@ def projectile_function(experimentalData:ExperimentalData):
 
 experimentalData = ExperimentalData('mosin', '7.62x54R LPS gzh', 'single', 865, 'Adventureland', 70, 9.81)
 
+# create a python class
+
 myDataSet = [
     ExperimentalData('mosin', '7.62x54R LPS gzh', 'single', 865, 'Adventureland', 70, 9.81),
     ExperimentalData('mosin', '7.62x54R LPS gzh', 'single', 865, 'World Trade Center', 415, 10.44),
@@ -45,7 +47,7 @@ myDataSet = [
     ExperimentalData('mosin', '7.62x54R LPS gzh', 'single', 865, 'Aura', 271, 1.62),
     ExperimentalData('mosin', '7.62x54R LPS gzh', 'single', 865, 'Shanghai Tower', 632, 24.79)
     ]
-
+#  create a for loop
 for data in myDataSet:
     print('\n---------------------------------------------------------\n')
     projectile_function(data)
@@ -56,4 +58,10 @@ myOutputFilePath = os.path.join(myOutputPath, 'Projectile-Motion.json')
 with open(myOutputFilePath, 'w') as outfile:
     json.dump([data.__dict__ for data in myDataSet], outfile)
 
+# DESERIALIZATION
+deserialize = open(myOutputFilePath)
+experimentJSON = json.load(deserialize)
+
+for e in experimentJSON:
+    ExperimentalData(**e)
 # projectile_function(experimentalData)
