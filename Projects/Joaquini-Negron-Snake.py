5ff30dd1254576a8a59781656786553b21ffe1a5
CSI-Joaquin-Negron <89479739+CSI-Joaquin-Negron@users.noreply.github.com>
2022-02-17 14:48:48 -0400

    Update Joaquini-Negron-Snake.py

--- Projects/Joaquini-Negron-Snake.py
+++ Projects/Joaquini-Negron-Snake.py
@@ -8,15 +8,44 @@ red = (255, 59, 91)# food
 green = (0, 80, 34)# background
 light_green = (0, 232, 101)# game over
 
-dis = pg.display.set_mode((600,500))
+
+dis = pg.display.set_mode((600,500)) #define screen resolution
+# pg.display.update
+
 pg.display.set_caption('Snake Game By JN')
 game_over = False
+
+x1 = 300
+y1 = 250
+
+x1_change = 0
+y1_change = 0
+
+clock = pg.time.Clock()
+
 while not game_over:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             game_over == True
-    pg.draw.rect(dis, purple, [300, 250, 10, 10])
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_LEFT:
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pg.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pg.K_UP:
+                y1_change = -10
+                x1_change = 0
+            elif event.key == pg.K_DOWN:
+                y1_change = 10
+                x1_change = 0
+    x1 += x1_change
+    y1 += y1_change
+    dis.fill(green)
+    pg.draw.rect(dis, purple, [x1, y1, 10, 10])
     pg.display.update()
+    clock.tick(70)
 pg.quit()
 
 quit()
