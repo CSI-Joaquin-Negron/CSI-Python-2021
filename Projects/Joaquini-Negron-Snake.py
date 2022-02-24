e5ec13a9ec6935896acd8e342de9d5665533fac1
CSI-Joaquin-Negron <89479739+CSI-Joaquin-Negron@users.noreply.github.com>
2022-02-24 15:01:24 -0400

    Update Joaquini-Negron-Snake.py

--- Projects/Joaquini-Negron-Snake.py
+++ Projects/Joaquini-Negron-Snake.py
@@ -1,5 +1,6 @@
 # create the screen
 import pygame as pg
+import time
 pg.init()
 
 yellow = (179, 178, 70)# score
@@ -9,19 +10,38 @@ green = (0, 80, 34)# background
 light_green = (0, 232, 101)# game over
 
 
-dis = pg.display.set_mode((600,500)) #define screen resolution
+dis_width = 600
+dis_height = 500
+dis = pg.display.set_mode((dis_width,dis_height)) #define screen resolution
 # pg.display.update
 
 pg.display.set_caption('Snake Game By JN')
 game_over = False
 
-x1 = 300
-y1 = 250
-
-x1_change = 0
-y1_change = 0
-
 clock = pg.time.Clock()
+snake_speed = 15
+
+snake_block = 10
+
+font_style = pg.font.SysFont(None,50)
+
+def message(msg,color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width/2, dis_height/2])
+
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    x1 = dis_width/2
+    y1 = dis_height/2
+    
+    x1_change = 0
+    y1_change = 0
+while not game_over:
+    while game_close == True:
+        dis.fill(green)
+         
 
 while not game_over:
     for event in pg.event.get():
@@ -29,23 +49,29 @@ while not game_over:
             game_over == True
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_LEFT:
-                x1_change = -10
+                x1_change = -snake_block
                 y1_change = 0
             elif event.key == pg.K_RIGHT:
-                x1_change = 10
+                x1_change = snake_block
                 y1_change = 0
             elif event.key == pg.K_UP:
-                y1_change = -10
+                y1_change = -snake_block
                 x1_change = 0
             elif event.key == pg.K_DOWN:
-                y1_change = 10
+                y1_change = snake_block
                 x1_change = 0
+    if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+        game_over = True
+
     x1 += x1_change
     y1 += y1_change
     dis.fill(green)
-    pg.draw.rect(dis, purple, [x1, y1, 10, 10])
+    pg.draw.rect(dis, purple, [x1, y1, snake_block, snake_block])
     pg.display.update()
-    clock.tick(70)
+    clock.tick(30)
+message("GAME OVER", red)
+pg.display.update
+time.sleep(5)
 pg.quit()
-
+pg.quit()
 quit()
